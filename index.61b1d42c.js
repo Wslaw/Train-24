ca38a974e456b08f39395a525c5accbfe8ff69d2
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-22 16:28:58 +0000

    Deploying to gh-pages from @ Wslaw/Train-24@4417d964d6cf1d9b688af3e2c3a798626cd5dca2 🚀

--- index.61b1d42c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".js-add"),t=(document.querySelector(" .js-posts"),document.querySelector(".js-form"));e.addEventListener("click",(function(){t.innerHTML='<form action="submit">\n    <input type="text" name="title">\n    <textarea name="body" id="" cols="30" rows="10"></textarea>\n    <button>Додати пост</button>\n    </form>'})),document.querySelector(".js-form-add").addEventListener("click",handlerFormSubmit)}();
-//# sourceMappingURL=index.61b1d42c.js.map
